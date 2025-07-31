7b8954cc6515f1c7684bc8b0ad7f7e1e0f14e176
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-07-31 05:52:39 +0300

    Convert to tabs

--- lua/weapons/gmod_tool/stools/unbreakable.lua
+++ lua/weapons/gmod_tool/stools/unbreakable.lua
@@ -1,81 +1,81 @@
 ﻿TOOL.Category = "Constraints"
 TOOL.Name = "Unbreakable"
 TOOL.Information = {
-    {name = "left"},
-    {name = "right"},
-    {name = "reload"}
+	{name = "left"},
+	{name = "right"},
+	{name = "reload"}
 }
 
 local function MakeUnbreakable(ply, ent, data)
-    if data.On then
-        ent.UnbreakableTool = data.On
-        duplicator.StoreEntityModifier(ent, "Unbreakable", data)
-    else
-        ent.UnbreakableTool = nil
-        duplicator.ClearEntityModifier(ent, "Unbreakable")
-    end
+	if data.On then
+		ent.UnbreakableTool = data.On
+		duplicator.StoreEntityModifier(ent, "Unbreakable", data)
+	else
+		ent.UnbreakableTool = nil
+		duplicator.ClearEntityModifier(ent, "Unbreakable")
+	end
 end
 
 if SERVER then
-    hook.Add("EntityTakeDamage", "UnbreakableTool", function(ent)
-        if ent:GetTable().UnbreakableTool then
-            return true
-        end
-    end)
+	hook.Add("EntityTakeDamage", "UnbreakableTool", function(ent)
+		if ent:GetTable().UnbreakableTool then
+			return true
+		end
+	end)
 
-    duplicator.RegisterEntityModifier("Unbreakable", MakeUnbreakable)
+	duplicator.RegisterEntityModifier("Unbreakable", MakeUnbreakable)
 end
 
 if CLIENT then
-    language.Add("tool.unbreakable.name", "Unbreakable")
-    language.Add("tool.unbreakable.desc", "Makes objects unbreakable")
-    language.Add("tool.unbreakable.left", "Make an object unbreakable")
-    language.Add("tool.unbreakable.right", "Restore object's previous settings")
-    language.Add("tool.unbreakable.reload", "Make the object and everything constrained to it unbreakable")
+	language.Add("tool.unbreakable.name", "Unbreakable")
+	language.Add("tool.unbreakable.desc", "Makes objects unbreakable")
+	language.Add("tool.unbreakable.left", "Make an object unbreakable")
+	language.Add("tool.unbreakable.right", "Restore object's previous settings")
+	language.Add("tool.unbreakable.reload", "Make the object and everything constrained to it unbreakable")
 end
 
 function TOOL:LeftClick(trace)
-    local ent = trace.Entity
+	local ent = trace.Entity
 
-    if IsValid(ent) then
-        if CLIENT then return true end
+	if IsValid(ent) then
+		if CLIENT then return true end
 
-        MakeUnbreakable(self:GetOwner(), ent, {On = true})
+		MakeUnbreakable(self:GetOwner(), ent, {On = true})
 
-        return true
-    end
+		return true
+	end
 
-    return false
+	return false
 end
 
 function TOOL:RightClick(trace)
-    local ent = trace.Entity
+	local ent = trace.Entity
 
-    if IsValid(ent) then
-        if CLIENT then return true end
+	if IsValid(ent) then
+		if CLIENT then return true end
 
-        MakeUnbreakable(self:GetOwner(), ent, {On = false})
+		MakeUnbreakable(self:GetOwner(), ent, {On = false})
 
-        return true
-    end
+		return true
+	end
 
-    return false
+	return false
 end
 
 function TOOL:Reload(trace)
-    local ent = trace.Entity
+	local ent = trace.Entity
 
-    if IsValid(ent) then
-        if CLIENT then return true end
+	if IsValid(ent) then
+		if CLIENT then return true end
 
-        local owner = self:GetOwner()
+		local owner = self:GetOwner()
 
-        for _, v in pairs(constraint.GetAllConstrainedEntities(ent)) do
-            MakeUnbreakable(owner, v, {On = true})
-        end
+		for _, v in pairs(constraint.GetAllConstrainedEntities(ent)) do
+			MakeUnbreakable(owner, v, {On = true})
+		end
 
-        return true
-    end
+		return true
+	end
 
-    return false
+	return false
 end
